50e97478555bf8b59293eed2249fe9632dca6595
JTomoyasu <30738319+JTomoyasu@users.noreply.github.com>
2018-02-15 23:28:13 -0800

    often

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ var questions=[];
 questions.push({quest:"Which of these songs is from the movie 'Mulan'?", a1:"I'll Make a Man Out of You", a2:"I Just Can't Wait to be King", a3:"Colors of the Wind",a4:"Be Our Guest",corrAns:"I'll Make a Man Out of You"});
 questions.push({quest:"Which of these characters is not from the movie 'Tarzan'?", a1:"Jane", a2:"Clayton", a3:"Turk",a4:"Gaston",corrAns:"Gaston"});
 questions.push({quest:"What book is the movie 'Treasure Planet' based on?", a1:"Moneyball", a2:"Robinson Crusoe", a3:"Treasure Island",a4:"The Martian Chronicles",corrAns:"Treasure Island"});
-questions.push({quest:"Which of these movies was not nominated for 'Best Picture' at the Academy Awards?", a1:"Toy Story 3", a2:"Beauty and the Beast (1991)", a3:"The Lion King",a4:"Up",corrAns:"The Lion King"});
+questions.push({quest:"Which of these movies was not nominated for 'Best Picture' at the Academy Awards?", a1:"Toy Story 3", a2:"Beauty and the Beast (1991)", a3:"Frozen",a4:"Up",corrAns:"Frozen"});
 questions.push({quest:"Which of these movies did not win 'Best Animated Feature' at the Academy Awards?", a1:"Beauty and the Beast (1991)", a2:"The Incredibles", a3:"Big Hero 6",a4:"Finding Nemo",corrAns:"Beauty and the Beast (1991)"});
 questions.push({quest:"Which of the following is not a quality a man must possess according to the song 'I'll Make a Man Out of You'?", a1:"Being as swift as a coursing river", a2:"Being as bright as the sun at high noon", a3:"Being as tranquil as a forest",a4:"Being mysterious as the dark side of the moon",corrAns:"Being as bright as the sun at high noon"});
 var count=0;
